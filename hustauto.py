303f9f48b8e08e91a2b027185bdd19cdf6e110ad
szrlee <szrlee@gmail.com>
2013-12-26 00:40:35 +0000

    the second commit

--- hustauto.py
+++ hustauto.py
@@ -1,28 +1,22 @@
 import urllib 
 import urllib2 
-import string
-
-def post(url, data): 
+import base64
+def post(url,data): 
     req = urllib2.Request(url) 
     data = urllib.urlencode(data) 
     #enable cookie 
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor()) 
-    response = opener.open(req, data) 
+    response = opener.open(req,data) 
     return response.read() 
-def encrypt(password){
-    str = password
-#    temp = new Array(password.length);
-#    for i in range(0,len(password))
-#        temp[i] = str.charCodeAt(i)^0xff
-#        ret += String.fromCharCode(temp[i]);
-    return ret;
 
 def main(): 
     posturl = "http://192.168.50.2:8080/portal/hust/desk/onlogin.jsp"
-    username = raw_input（'username'）
+    username = raw_input('username')
     password = raw_input('password')
+    ret = base64.encodestring(password)
     data={'userName='+username+'&userPwd='+ret}
-    print post(posturl, data) 
+    print ret
+    print post(posturl,data) 
                        
 if __name__ == '__main__': 
     main()  
